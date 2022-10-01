dfdded82aa8ee662110804d1f18fde684704fff4
patriksinox <patrik.subjak1502@gmail.com>
2022-10-01 17:14:42 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement } from "react";
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+  const [ludia, setLudia] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section className="container">
+        <h1>{ludia.length} narodenín dnes.</h1>
+        {ludia.map((osoba) => {
+          const { id, meno, vek, fotka } = osoba;
+          return (
+            <article className="ramik" key={id}>
+              <img className="fotka" src={fotka} alt="osoba" />
+              <div>
+                <h5>{meno}</h5>
+                <h6>{vek} rokov.</h6>
+              </div>
+            </article>
+          );
+        })}
+        <div className="text-center">
+          <button className="btn btn-primary" onClick={() => setLudia([])}>
+            Vymaž narodeniny
+          </button>
+        </div>
+      </section>
+    </React.Fragment>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    id: 1502,
+    meno: "Patrik Šubjak",
+    vek: 26,
+    fotka: "./patrik.jpg",
+  },
+  {
+    id: 1,
+    meno: "Bertie Yates",
+    vek: 29,
+    fotka:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    meno: "Hester Hogan",
+    vek: 32,
+    fotka:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    meno: "Larry Little",
+    vek: 36,
+    fotka:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    meno: "Sean Walsh",
+    vek: 34,
+    fotka:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    meno: "Lola Gardner",
+    vek: 29,
+    fotka:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
